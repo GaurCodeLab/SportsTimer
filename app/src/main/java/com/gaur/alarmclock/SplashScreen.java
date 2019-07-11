fcbc339ea69c173219693043c0da28dfb2381aeb
iamgaur <ankit.gaur1023@gmail.com>
2019-07-11 13:58:28 +0530

    Animations in splash Screen

--- app/src/main/java/com/gaur/alarmclock/SplashScreen.java
+++ app/src/main/java/com/gaur/alarmclock/SplashScreen.java
@@ -5,13 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+
+    Animation frombottom, fromtop;
+    ImageView logo;
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        title = findViewById(R.id.logoText);
+        logo = findViewById(R.id.logoImage);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.alpha);
+        frombottom = AnimationUtils.loadAnimation(this,R.anim.translate);
+        title.setAnimation(frombottom);
+        logo.setAnimation(fromtop);
 
 
         Handler handler = new Handler();
@@ -24,6 +40,9 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
 
             }
-        }, 500);
+        }, 2500);
     }
+
+
+
 }
